144b32aea1765f0216c95025faea43c8781b72c8
Pedro Melo <melo@simplicidade.org>
2008-10-09 17:26:49 +0100

    Cleanup basic version of CORM.pm
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- DBICx-CORM/lib/DBICx/CORM.pm
+++ DBICx-CORM/lib/DBICx/CORM.pm
@@ -3,62 +3,76 @@ package DBICx::CORM;
 use warnings;
 use strict;
 
+our $VERSION = '0.01';
+
+42; # End of DBICx::CORM
+
 =head1 NAME
 
-DBICx::CORM - The great new DBICx::CORM!
+DBICx::CORM - A alternative ruleset for DBIx::Class-based ORMs
+
 
 =head1 VERSION
 
 Version 0.01
 
-=cut
-
-our $VERSION = '0.01';
-
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+    # This is the class you'll use as a frontend
+    package My::S;
+    
+    use strict;
+    use warnings;
+    use base qw( DBIXc::CORM::Shortcuts );
+    
+    __PACKAGE__->setup({
+      ...
+    });
+    
+    1;
+    
+    # the classic Schema-based setup
+    package My::Schema;
+    
+    use strict;
+    use warnings;
+    use base qw( DBICx::CORM::Schema );
+    
+    __PACKAGE__->load_namespaces;
+    
+    1;
+    
+    # Your table classes will be at My::Schema::Result::*
+    # and My::Schema::ResultSet::*
+    #
+    
+    package My::Schema::Result::TableA;
+    
+    use strict;
+    use warnings;
+    use base qw( DBICx::CORM::Source );
+    
+    # table declaration goes here
+    
+    1;
 
-Perhaps a little code snippet.
 
-    use DBICx::CORM;
+=head1 DESCRIPTION
 
-    my $foo = DBICx::CORM->new();
-    ...
+TBW
 
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 FUNCTIONS
-
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
 
 =head1 AUTHOR
 
 Pedro Melo, C<< <melo at cpan.org> >>
+
 
 =head1 BUGS
 
 Please report any bugs or feature requests to C<bug-dbicx-corm at rt.cpan.org>, or through
 the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=DBICx-CORM>.  I will be notified, and then you'll
 automatically be notified of progress on your bug as I make changes.
-
-
 
 
 =head1 SUPPORT
@@ -96,12 +110,10 @@ L<http://search.cpan.org/dist/DBICx-CORM>
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2008 Pedro Melo, all rights reserved.
+Copyright 2008 Pedro Melo.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
 
 =cut
-
-1; # End of DBICx::CORM
